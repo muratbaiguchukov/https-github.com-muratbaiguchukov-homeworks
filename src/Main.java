7b0185410e3f398ed5722a92d476ffc370aae885
muratbaiguchukov <ambkg-muratb@yandex.ru>
2022-03-15 17:29:53 +0600

    add classwork-49-jdbc

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.sql.*;
+import java.sql.Connection;
+
+public class Main {
+    private static final String url = "jdbc:postgresql://localhost:5433/";
+    private static final String user = "postgres";
+    private static final String password = "master";
+
+    public static void main(String[] args) {
+        connect();
+    }
+
+    public static Connection connect(){
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    public static int getTreneryCount() {
+        String SQL = "SELECT count(*) FROM trenery";
+        int count = 0;
+        try (Connection conn = connect();
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery(SQL)) {
+            count = rs.getInt(1);
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+            return count;
+        }
+
+}
